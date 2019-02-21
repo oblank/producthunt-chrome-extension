b7d884fc9bf654597c8d90f165f286b597f11d01
oBlank <dyh1919@gmail.cm>
2019-02-21 19:25:07 +0800

    github module

--- src/apps/background/xframe.js
+++ src/apps/background/xframe.js
@@ -6,11 +6,25 @@
  * x-frame option, but that will do too.
  */
 
-chrome.webRequest.onHeadersReceived.addListener(function(details) {
-  for (var i = 0; i < details.responseHeaders.length; ++i) {
-    if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
-      details.responseHeaders.splice(i, 1);
-      return { responseHeaders: details.responseHeaders };
+chrome.webRequest.onHeadersReceived.addListener(function (details) {
+    let tmp = {}
+    for (var i = 0; i < details.responseHeaders.length; ++i) {
+        if (details.responseHeaders[ i ].name.toLowerCase() == 'content-security-policy') {
+            details.responseHeaders[ i ].value = '';
+        }
+        if (details.responseHeaders[ i ].name.toLowerCase() == 'strict-transport-security') {
+            details.responseHeaders[ i ].value = '';
+        }
+        // X-XSS-Protection
+        if (details.responseHeaders[ i ].name.toLowerCase() == 'x-xss-protection') {
+            details.responseHeaders[ i ].value = '';
+        }
+        if (details.responseHeaders[ i ].name.toLowerCase() == 'x-frame-options') {
+            details.responseHeaders.splice(i, 1);
+            // tmp = details.responseHeaders;
+            // break;
+        }
     }
-  }
-}, { urls: ['*://www.producthunt.com/*'] }, ['blocking', 'responseHeaders']);
+    return { responseHeaders: details.responseHeaders };
+}, { urls: [ "*://*/*" ], types: [ "main_frame", "sub_frame" ] }, [ 'blocking', 'responseHeaders' ]);
+
--- src/apps/tabs/components/Github.react.js
+++ src/apps/tabs/components/Github.react.js
@@ -106,7 +106,7 @@ let Github = React.createClass({
                 if (res.status !== 200) {
                     console.error(res);
                 } else {
-                    console.log(res.body);
+                    // console.log(res.body);
                     this.setState({ posts: res.body })
                 }
             });
@@ -118,19 +118,37 @@ let Github = React.createClass({
 
     render() {
         let url = this.state.url;
-
+        let index = 0;
         let posts = this.state.posts.map((repo) => {
-            return <div key={repo.name} onClick={() => {
+            index++;
+
+            return <div className="repo-item clickable" key={repo.name} onClick={() => {
                 this._openPane(repo.url)
             }}>
-                <div id="repo-title">{repo.name}</div>
-                <div id="repo-info">
-                    {repo.description}
-                </div>
-                <div id="repo-info">
-                    <div id="">{repo.stars}</div>
-                    <div id="">{repo.fork}</div>
-                    <div id="">{repo.currentPeriodStars}</div>
+                <div className="repo-title"><span className="repo-index">{index}.</span>{repo.name}</div>
+                <div className="repo-info">{repo.description}</div>
+                <div className="repo-stars">
+                    <div className="repo-star">
+                        <svg aria-label="star" className="octicon octicon-star" viewBox="0 0 14 16" version="1.1" width="14" height="16"
+                             role="img">
+                            <path fill-rule="evenodd"
+                                  d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path>
+                        </svg>
+                        {repo.stars}
+                    </div>
+                    <div className="repo-star">
+                        <svg aria-label="fork" className="octicon octicon-repo-forked" viewBox="0 0 10 16" version="1.1" width="10" height="16"
+                             role="img">
+                            <path fill-rule="evenodd"
+                                  d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path>
+                        </svg>
+                        {repo.forks}
+
+                    </div>
+                    <div className="repo-star">
+
+                        + {repo.currentPeriodStars}
+                    </div>
                 </div>
             </div>
         });
@@ -150,6 +168,7 @@ let Github = React.createClass({
                 </div>
 
                 <Pane
+                    style={{ background: "#FFF" }}
                     bodyClass="no-scroll"
                     loaderClass="loader"
                     overlayClass="overlay"
@@ -170,6 +189,7 @@ let Github = React.createClass({
     _openPane(url) {
         debug("[-]open pane with url", url);
         this.setState({ url: url });
+        // this.setState({ url: 'https://baidu.com' });
     },
 
     /**
