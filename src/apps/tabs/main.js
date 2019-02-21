06789df2ed29a88592dc66116820d3df7261c92a
oBlank <dyh1919@gmail.cm>
2019-02-21 14:40:23 +0800

    update display logic

--- src/apps/tabs/main.js
+++ src/apps/tabs/main.js
@@ -3,11 +3,15 @@
  */
 
 let React = require('react');
+const ReactDOM = require('react-dom');
+const ReactMarkdown = require('react-markdown');
+var $ = require("jquery");
 let debug = require('debug')('ph:tab:main');
 let DefaultTab = require('./components/DefaultTab.react');
 let renderComponent = require('../../common/render');
 let loadGoogleAnalytics = require('../../common/google-analytics');
 let settings = require('../../common/settings');
+
 
 /**
  * Constants.
@@ -17,3 +21,30 @@ const GA_ID = process.env.GA_ID;
 
 loadGoogleAnalytics(GA_ID);
 renderComponent(<DefaultTab />, document.getElementById('main'));
+
+/**
+ * render notes
+ * @type {string}
+ */
+const input = '- Fabric网络搭建测试 \n - 测试环境接口-IP优化 \n - 监控增强，shell自动重启？';
+
+ReactDOM.render(
+    <ReactMarkdown source={input} />,
+    document.getElementById('notes')
+);
+
+
+/**
+ * double click note to edit
+ * @type {*|jQuery|HTMLElement}
+ */
+$( "#notes" ).dblclick(function() {
+    $( "#notes" ).fadeOut();
+    $( "#editor" ).fadeIn();
+    // $('#noteEditor').focus();
+});
+
+$("#noteEditor").blur(function () {
+    $( "#notes" ).fadeIn();
+    $( "#editor" ).fadeOut();
+});
